b2b342f6c7a196d87c83a34527122be642113777
Rinoy joseph <rinoykj007@example.com>
2025-03-06 10:02:21 +0000

    update Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>
         </main>
-        <Footer />
       </div>
     </Router>
   );
