6a88f0284b42ac9cd3224c3572e30a4477b64dd4
Rinoy joseph <rinoykj007@example.com>
2025-03-06 10:03:57 +0000

    update about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </main>
       </div>
