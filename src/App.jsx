909fc7eecacf6ba1e8021495c8e4bc7bcb73a865
Rinoy joseph <rinoykj007@example.com>
2025-03-29 21:10:33 +0000

    Resolve import issues for Blog, BlogAdmin, and Footer in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Blog from "./pages/Blog";
+import BlogAdmin from "./pages/admin/BlogAdmin";
+import Footer from "./components/Footer";
 
 function App() {
   return (
